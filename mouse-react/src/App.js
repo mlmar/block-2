94a5c76884aa9a826b86872cf63ed60f6741f55f
mlmar <marcloussm@gmail.com>
2021-08-14 17:45:49 -0700

    fastclick

--- mouse-react/src/App.js
+++ mouse-react/src/App.js
@@ -1,13 +1,19 @@
 import './css/main.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import FastClick from 'fastclick';
 
 import Home from './js/modules/Home.js';
 import Room from './js/modules/Room.js';
 
 const App = () => {
   const [name, setName] = useState(null); // user's display name
+
+  useEffect(() => {
+    FastClick.attach(document.body);
+  }, [])
+
   return (
     <div className="app">
       <BrowserRouter>
